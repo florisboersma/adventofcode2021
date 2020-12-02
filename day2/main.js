6da53b5cdcf012532bd897554c30bb73b9c6ded9
Floris Boersma <floris.boersma@incentro.com>
2020-12-02 20:22:14 +0100

    Fix: Solution for part two of day 2.

--- day2/main.js
+++ day2/main.js
@@ -6,19 +6,38 @@ let validPasswords = 0;
 formattedInput.forEach((passwordLine) => {
   const singleLineArray = passwordLine.split(" ");
 
-  const amountArray = singleLineArray[0].split("-");
+  // _____ THIS IS THE ANSWER FOR PART ONE OF THE FIRST DAY: _____
+  // const amountArray = singleLineArray[0].split("-");
+  // const validationLetter = singleLineArray[1].charAt(0);
+  // const password = singleLineArray[2];
+
+  // const minimumAmount = amountArray[0];
+  // const maximumAmount = amountArray[1];
+  // const characterRegex = new RegExp(`${validationLetter}`, "g");
+  // const characterAmount = (password.match(characterRegex) || []).length;
+  // console.log(characterAmount);
+
+  // if (characterAmount >= minimumAmount && characterAmount <= maximumAmount) {
+  //   validPasswords++;
+  // }
+  // _____________________________________________________________
+
+  const positions = singleLineArray[0].split("-");
   const validationLetter = singleLineArray[1].charAt(0);
   const password = singleLineArray[2];
 
-  const minimumAmount = amountArray[0];
-  const maximumAmount = amountArray[1];
+  const firstPosition = parseInt(positions[0]) - 1;
+  const secondPosition = parseInt(positions[1]) - 1;
 
-  const characterRegex = new RegExp(`${validationLetter}`, "g");
+  const firstPositionCheck =
+    password.charAt(firstPosition) === validationLetter;
+  const secondPositionCheck =
+    password.charAt(secondPosition) === validationLetter;
 
-  const characterAmount = (password.match(characterRegex) || []).length;
-  console.log(characterAmount);
-
-  if (characterAmount >= minimumAmount && characterAmount <= maximumAmount) {
+  if (
+    (firstPositionCheck || secondPositionCheck) &&
+    !(firstPositionCheck && secondPositionCheck)
+  ) {
     validPasswords++;
   }
 });
