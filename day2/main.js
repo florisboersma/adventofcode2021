e29892d4c0332950033863ab1c3754c53e33b64d
Floris Boersma <floris.boersma@incentro.com>
2020-12-02 20:04:52 +0100

    Fix: Solution for part one of day 2.

--- day2/main.js
+++ day2/main.js
@@ -1,3 +1,26 @@
 import { input } from "./input.js";
 
-console.log(input);
+const formattedInput = input.split(/\r?\n/);
+let validPasswords = 0;
+
+formattedInput.forEach((passwordLine) => {
+  const singleLineArray = passwordLine.split(" ");
+
+  const amountArray = singleLineArray[0].split("-");
+  const validationLetter = singleLineArray[1].charAt(0);
+  const password = singleLineArray[2];
+
+  const minimumAmount = amountArray[0];
+  const maximumAmount = amountArray[1];
+
+  const characterRegex = new RegExp(`${validationLetter}`, "g");
+
+  const characterAmount = (password.match(characterRegex) || []).length;
+  console.log(characterAmount);
+
+  if (characterAmount >= minimumAmount && characterAmount <= maximumAmount) {
+    validPasswords++;
+  }
+});
+
+console.log("Amount of valid passwords: ", validPasswords);
