ac091dfc1c0347600a5f0f6b463c08b8d33ab1a2
Floris Boersma <floris.boersma@incentro.com>
2020-12-03 08:52:40 +0100

    work: Working on part two of day two, not the right solution yet.

--- day3/main.js
+++ day3/main.js
@@ -12,8 +12,8 @@ const repeatedMap = formattedInput.map((row) => {
   return repeatedRow;
 });
 
-let amountOfTrees = 0;
 // _____ THIS IS THE ANSWER FOR PART ONE OF THE FIRST DAY: _____
+// let amountOfTrees = 0;
 // let rowCounter = 0;
 // let columnCounter = 0;
 
@@ -28,6 +28,52 @@ let amountOfTrees = 0;
 //     rowCounter++;
 //   }
 // });
+
+// console.log(amountOfTrees);
 // _____________________________________________________________
 
+let amountOfTrees = [0, 0, 0, 0, 0];
+let columnIncrement = [1, 3, 5, 7, 1];
+let rowIncrement = [1, 1, 1, 1, 2];
+
+for (let i = 0; i < 5; i++) {
+  let currentAmount = 0;
+  let rowCounter = 0;
+  let columnCounter = 0;
+
+  repeatedMap.forEach((row) => {
+    if (rowIncrement[i] === 1) {
+      if (rowCounter !== 0) {
+        columnCounter += columnIncrement[i];
+
+        if (row.charAt(columnCounter) === "#") {
+          currentAmount++;
+        }
+      } else {
+        rowCounter += rowIncrement[i];
+      }
+    } else {
+      columnCounter += columnIncrement[i];
+
+      if (rowCounter % 2 === 0) {
+        if (row.charAt(columnCounter) === "#") {
+          currentAmount++;
+        }
+      }
+      rowCounter++;
+    }
+  });
+
+  amountOfTrees[i] = currentAmount;
+}
+
 console.log(amountOfTrees);
+
+const totalTrees =
+  amountOfTrees[0] *
+  amountOfTrees[1] *
+  amountOfTrees[2] *
+  amountOfTrees[3] *
+  amountOfTrees[4];
+
+console.log(totalTrees);
