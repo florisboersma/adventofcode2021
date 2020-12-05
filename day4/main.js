d230cd4ff6972d82acb1f66dfcd4253e9e674687
Floris Boersma <floris.boersma@incentro.com>
2020-12-05 11:13:01 +0100

    Initial work on part two of day four, not done yet.

--- day4/main.js
+++ day4/main.js
@@ -39,6 +39,7 @@ console.log(allFormattedPassports);
 
 let validPassports = 0;
 allFormattedPassports.forEach((passport) => {
+  let passportIsValid = true;
   let allRequiredFields = false;
 
   if (passport.length === 8) {
@@ -63,7 +64,85 @@ allFormattedPassports.forEach((passport) => {
 
   // only execute validation if all required fields are present
   if (allRequiredFields) {
-    // Work on this puzzle here
+    passport.forEach((passportLine) => {
+      // Check for birth year
+      if (passportLine.substring(0, 3) === "byr") {
+        const data = passportLine.substring(4);
+
+        if (parseInt(data) < 1920 || parseInt(data) > 2002) {
+          passportIsValid = false;
+        }
+      }
+
+      // Check for issue year
+      if (passportLine.substring(0, 3) === "iyr") {
+        const data = passportLine.substring(4);
+
+        if (parseInt(data) < 2010 || parseInt(data) > 2020) {
+          passportIsValid = false;
+        }
+      }
+
+      // Check for expiration year
+      if (passportLine.substring(0, 3) === "eyr") {
+        const data = passportLine.substring(4);
+
+        if (parseInt(data) < 2020 || parseInt(data) > 2030) {
+          passportIsValid = false;
+        }
+      }
+
+      // Check for height
+      if (passportLine.substring(0, 3) === "eyr") {
+        const data = passportLine.substring(4);
+
+        if (data.slice(-2) === "cm") {
+          if (
+            parseInt(data.slice(0, -2)) < 150 ||
+            parseInt(data.slice(0, -2)) > 193
+          ) {
+            passportIsValid = false;
+          }
+        } else {
+          if (
+            parseInt(data.slice(0, -2)) < 59 ||
+            parseInt(data.slice(0, -2)) > 76
+          ) {
+            passportIsValid = false;
+          }
+        }
+      }
+
+      // check for hair color
+      if (passportLine.substring(0, 3) === "hcl") {
+        const data = passportLine.substring(4);
+        const regex = new RegExp(`#([a-fA-F0-9]{6})$`);
+
+        if (!regex.test(data)) {
+          passportIsValid = false;
+        }
+      }
+
+      // check for eye color
+      if (passportLine.substring(0, 3) === "ecl") {
+        const data = passportLine.substring(4);
+        const allEyeColours = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+
+        if (!allEyeColours.indexOf(data) > -1) {
+          passportIsValid = false;
+        }
+      }
+
+      // check for passport ID
+      if (passportLine.substring(0, 3) === "pid") {
+        const data = passportLine.substring(4);
+        const allEyeColours = ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"];
+
+        if (!allEyeColours.indexOf(data) > -1) {
+          passportIsValid = false;
+        }
+      }
+    });
   }
 });
 
