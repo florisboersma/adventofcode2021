16c6d40d829e62205acc933cc1740c06a3eae749
Floris Boersma <floris.boersma@incentro.com>
2020-12-05 10:49:14 +0100

    Initial work on part two of day four.

--- day4/main.js
+++ day4/main.js
@@ -10,13 +10,41 @@ formattedInput.forEach((inputLine) => {
 
 console.log(allFormattedPassports);
 
+// _____ THIS IS THE ANSWER FOR PART ONE OF THE FIRST DAY: _____
+// let validPassports = 0;
+// allFormattedPassports.forEach((passport) => {
+//   // if all fields are present, passport is always valid
+//   if (passport.length === 8) {
+//     validPassports++;
+//   } else {
+//     // if the only missing field is CID, the passport is still valid.
+//     if (passport.length === 7) {
+//       let hasCID = false;
+
+//       // check if CID is in the passport
+//       passport.forEach((passportLine) => {
+//         if (passportLine.substring(0, 3) === "cid") {
+//           hasCID = true;
+//         }
+//       });
+
+//       // if CID is in the passport, this means some other field is missing, making the passport unvalid
+//       if (!hasCID) {
+//         validPassports++;
+//       }
+//     }
+//   }
+// });
+// _____________________________________________________________
+
 let validPassports = 0;
 allFormattedPassports.forEach((passport) => {
-  // if all fields are present, passport is always valid
+  let allRequiredFields = false;
+
   if (passport.length === 8) {
-    validPassports++;
+    allRequiredFields = true;
   } else {
-    // if the only missing field is CID, the passport is still valid.
+    // if the only missing field is CID, the passport has all required fields.
     if (passport.length === 7) {
       let hasCID = false;
 
@@ -27,11 +55,15 @@ allFormattedPassports.forEach((passport) => {
         }
       });
 
-      // if CID is in the passport, this means some other field is missing, making the passport unvalid
       if (!hasCID) {
-        validPassports++;
+        allRequiredFields = true;
       }
     }
+  }
+
+  // only execute validation if all required fields are present
+  if (allRequiredFields) {
+    // Work on this puzzle here
   }
 });
 
