6e22ffbf9969b521eea9fc3c44f9657fc597e32c
Floris Boersma <floris.boersma@incentro.com>
2020-12-05 10:44:34 +0100

    Fix: Solution for part one of day four.

--- day4/main.js
+++ day4/main.js
@@ -1,5 +1,38 @@
-import { input } from "./testInput.js";
+import { input } from "./input.js";
 
-const formattedInput = input.split(/\r?\n/);
+const formattedInput = input.split("\n\n");
+let allFormattedPassports = [];
 
-console.log(formattedInput);
+formattedInput.forEach((inputLine) => {
+  const formattedPassport = inputLine.replace(/\n/g, " ").split(" ");
+  allFormattedPassports.push(formattedPassport);
+});
+
+console.log(allFormattedPassports);
+
+let validPassports = 0;
+allFormattedPassports.forEach((passport) => {
+  // if all fields are present, passport is always valid
+  if (passport.length === 8) {
+    validPassports++;
+  } else {
+    // if the only missing field is CID, the passport is still valid.
+    if (passport.length === 7) {
+      let hasCID = false;
+
+      // check if CID is in the passport
+      passport.forEach((passportLine) => {
+        if (passportLine.substring(0, 3) === "cid") {
+          hasCID = true;
+        }
+      });
+
+      // if CID is in the passport, this means some other field is missing, making the passport unvalid
+      if (!hasCID) {
+        validPassports++;
+      }
+    }
+  }
+});
+
+console.log("amount of valid passports: ", validPassports);
